6dca435ced6f0d6efb096803621f687109f1ba98
lagoshny <ilya@lagoshny.ru>
2020-07-16 12:10:32 +0300

    Added URL utils

--- projects/ngx-hal-client/src/lib/util/url.utils.ts
+++ projects/ngx-hal-client/src/lib/util/url.utils.ts
@@ -3,19 +3,28 @@ import { ObjectUtils } from './object.utils';
 
 export class UrlUtils {
 
-    private static readonly URL_TEMPLATE_VAR_REGEXP = /{[^}]*}/g;
-    private static readonly EMPTY_STRING = '';
+  private static readonly URL_TEMPLATE_VAR_REGEXP = /{[^}]*}/g;
+  private static readonly EMPTY_STRING = '';
 
-    public static removeUrlTemplateVars(srcUrl: string) {
-        return srcUrl.replace(UrlUtils.URL_TEMPLATE_VAR_REGEXP, UrlUtils.EMPTY_STRING);
-    }
+  public static removeUrlTemplateVars(srcUrl: string) {
+    return srcUrl.replace(UrlUtils.URL_TEMPLATE_VAR_REGEXP, UrlUtils.EMPTY_STRING);
+  }
 
-    public static addSlash(uri: string): string {
-        const uriParsed = url.parse(uri);
-        if (ObjectUtils.isNullOrUndefined(uriParsed.search) && uri && uri[uri.length - 1] !== '/') {
-            return uri + '/';
+  public static convertToHttpParams(params: HalParam, httpParams?: HttpParams): HttpParams {
+    let resultParams = httpParams ? httpParams : new HttpParams();
+    if (_.isObject(params)) {
+      for (const [key, value] of Object.entries(params)) {
+        if (params.hasOwnProperty(key)) {
+          const paramValue = isResource(value)
+            ? value.getSelfLinkHref()
+            : value.toString();
+
+          resultParams = resultParams.append(key, paramValue);
         }
-        return uri;
+      }
     }
+
+    return resultParams;
+  }
 
 }

9b36cae22b7390869ea8624ec2cd9bd1998f5bd7
Lagoshny Ilya <ilya@lagoshny.ru>
2020-08-22 12:08:39 +0300

    Implement cache service

--- projects/ngx-hateoas-client/src/lib/service/cache.service.ts
+++ projects/ngx-hateoas-client/src/lib/service/cache.service.ts
@@ -1,30 +1,79 @@
 import { Injectable } from '@angular/core';
 import { ResourceIdentifiable } from '../model/resource/resource-identifiable';
+import { CacheResource } from '../model/cache/cache-resource';
+import { StageLogger } from '../logger/stage-logger';
+import { Stage } from '../logger/stage.enum';
+import { CacheKey } from '../model/cache/cache-key';
+import * as _ from 'lodash';
 
-// TODO: should be implemented
 @Injectable()
 export class CacheService<T extends ResourceIdentifiable> {
 
-  public hasResource(url: string): boolean {
-    return false;
+  /**
+   * Time before cache was expired (seconds).
+   */
+  public static expireTime: number = 5 * 60 * 1000;
+
+  public static enabled: boolean;
+
+  private cacheMap: Map<string, CacheResource<T>> = new Map<string, CacheResource<T>>();
+
+  /**
+   * Get cache value.
+   *
+   * @param key cache key
+   * @return cached value or {@code null} when cached value is not exist or expired
+   */
+  public getValue(key: CacheKey): T {
+    if (!CacheService.enabled) {
+      return null;
+    }
+    const cacheResource = this.cacheMap.get(key.value);
+    if (_.isNil(cacheResource)) {
+      StageLogger.stageLog(Stage.CACHE_GET, {cacheKey: key.value, result: null});
+      return null;
+    }
+
+    const resourceExpiredTime = new Date(cacheResource.cachedTime);
+    resourceExpiredTime.setSeconds(resourceExpiredTime.getSeconds() + CacheService.expireTime);
+    if (resourceExpiredTime.getTime() < new Date().getTime()) {
+      this.cacheMap.delete(key.value);
+      StageLogger.stageLog(Stage.CACHE_GET, {cacheKey: key.value, message: 'cache was expired', result: null});
+      return null;
+    }
+
+    StageLogger.stageLog(Stage.CACHE_GET, {cacheKey: key.value, result: cacheResource.resource});
+    return cacheResource.resource;
   }
 
-  public getResource(): T {
-    return null;
+  /**
+   * Add value to the cache.
+   * Before add new value, previous will be deleted if it was exist.
+   *
+   * @param key cache key
+   * @param resource cache value
+   */
+  public putValue(key: CacheKey, resource: T): void {
+    if (!CacheService.enabled) {
+      return;
+    }
+    this.cacheMap.delete(key.value);
+    this.cacheMap.set(key.value, new CacheResource<T>(resource, new Date()));
+
+    StageLogger.stageLog(Stage.CACHE_PUT, {cacheKey: key.value, resource});
   }
 
-  public putResource(url: string, resource: T) {
+  /**
+   * Delete cache value by passed key.
+   *
+   * @param key cache key
+   */
+  public evictValue(key: CacheKey): void {
+    if (!CacheService.enabled) {
+      return;
+    }
+    const result = this.cacheMap.delete(key.value);
+    StageLogger.stageLog(Stage.CACHE_EVICT, {cacheKey: key.value, evicted: result});
   }
-
-  public evictResource(url: string) {
-  }
-
-  public getResourceCollection(): T {
-    return null;
-  }
-
-  public putResourceCollection(url: string, resource: T) {
-  }
-
 
 }

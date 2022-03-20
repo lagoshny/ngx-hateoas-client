31cd05c6438af9da65b36d045f78425ab2352dbf
lagoshny <ilya@lagoshny.ru>
2022-03-20 17:10:02 +0300

    ISSUE-43 export additional interfaces
    (cherry picked from commit 9fbc83302c5c4f8457700b0ece551b3f6bbdb305)

--- projects/ngx-hateoas-client/src/lib/ngx-hateoas-client.module.ts
+++ projects/ngx-hateoas-client/src/lib/ngx-hateoas-client.module.ts
@@ -13,7 +13,7 @@ export { Resource } from './model/resource/resource';
 export { EmbeddedResource } from './model/resource/embedded-resource';
 export { ResourceCollection } from './model/resource/resource-collection';
 export { PagedResourceCollection } from './model/resource/paged-resource-collection';
-export { Sort, SortOrder, Include, HttpMethod, ProjectionRelType } from './model/declarations';
+export { Sort, SortOrder, Include, HttpMethod, ProjectionRelType, GetOption, PagedGetOption, RequestOption, RequestParam } from './model/declarations';
 export { HateoasResourceOperation } from './service/external/hateoas-resource-operation';
 export { HateoasResourceService } from './service/external/hateoas-resource.service';
 export { HateoasResource, HateoasEmbeddedResource, HateoasProjection, ProjectionRel } from './model/decorators';
